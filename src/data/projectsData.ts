94e3a6e0cd32de01cb94936c9a3650d9d6c9d07b
kmkkkp <kmkkkp@ajou.ac.kr>
2025-05-26 09:16:22 +0900

    FIX project liveurl or git

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -20,28 +20,33 @@ export const projectsData: ProjectItem[] = [
     description: '앞서 진행한 쌍방향 봉사활동 매칭 플랫폼의 관리자 콘솔 개발',
     image: 'https://images.pexels.com/photos/3183150/pexels-photo-3183150.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     technologies: ['Docker', 'Node.js', 'Sequelize'],
-    period: 'Mar 2024 – Jun 2024'
+    period: 'Mar 2024 – Jun 2024',
+    liveUrl: 'https://www.youtube.com/watch?v=P41qr2NpVes',
   },
   {
     title: '응급차 사전 알림 시스템',
     description: '응급차량의 빠른 도착을 위한 사전 알림 시스템 개발, Google Solution Challenge 2024 Top 100 수상',
     image: 'https://images.pexels.com/photos/1677160/pexels-photo-1677160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     technologies: ['Google Cloud Platform', 'Flutter'],
-    period: 'Jan 2024 – Feb 2024'
+    period: 'Jan 2024 – Feb 2024',
+    liveUrl: 'https://www.youtube.com/watch?v=fXNNaIofH3A',
+    repoUrl: 'https://github.com/Ajou-Soft-19/epas',
   },
   {
     title: 'Gitstagram',
     description: '교내 커뮤니티 활성화를 목표로 Gitlab과 연동한 SNS 데모웹 개발, 아주톤 장려상 수상',
     image: 'https://images.pexels.com/photos/4549416/pexels-photo-4549416.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     technologies: ['React', 'TypeScript'],
-    period: 'Oct 28 – Oct 29, 2023'
+    period: 'Oct 28 – Oct 29, 2023',
+    liveUrl: 'https://www.youtube.com/watch?v=DZgT-WxW9bA',
   },
   {
     title: '쌍방향 봉사활동 매칭 플랫폼 웹앱',
     description: '원하는 봉사활동을 붕사자와 요구자가 신청하고, 매칭할 수 있는 플랫폼 개발',
     image: 'https://images.pexels.com/photos/7014337/pexels-photo-7014337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
     technologies: ['Docker', 'React', 'TypeScript', 'Flutter'],
-    period: 'Sep 2023 – Dec 2024'
+    period: 'Sep 2023 – Dec 2024',
+    liveUrl: 'https://www.youtube.com/watch?v=wtwZimAIpWA',
   },
   {
     title: 'mdn 사이트 번역',
